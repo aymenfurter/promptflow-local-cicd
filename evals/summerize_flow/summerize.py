a9244ce1ff756b07a089c71bf853ffa15c8e9ac5
Aymen Furter <aymen.furter@outlook.com>
2023-12-30 11:26:31 +0000

    fix: incorrect reference

--- evals/summerize_flow/summerize.py
+++ evals/summerize_flow/summerize.py
@@ -2,9 +2,9 @@ from promptflow import tool
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-from summarizer import summarize_text
+import summarizer
 
 
 @tool
 def test_summarize(text: str) -> str:
-    return summerize_text(text)
+    return summarizer.summerize_text(text)
