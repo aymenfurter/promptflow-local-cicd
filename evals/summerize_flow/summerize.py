1485f63af85055b092b9faa4b11d10c351143b91
Aymen Furter <aymen.furter@outlook.com>
2023-12-30 10:38:50 +0000

    feat: initial commit

---
+++ evals/summerize_flow/summerize.py
@@ -0,0 +1,9 @@
+from promptflow import tool
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from summarizer import summarize_text
+
+
+@tool
+def test_summarize(text: str) -> str:
+    return summerize_text(text)
